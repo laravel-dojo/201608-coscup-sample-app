1fc6bcff4585b88aa3b3f2e92279cee5857307fa
Shengyou Fan <shengyoufan@gmail.com>
2016-05-14 22:36:34 +0800

    查詢所有 Task 資料並送到 View 顯示

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,19 +6,16 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Task;
+
 class TasksController extends Controller
 {
     public function index()
     {
-        $tasks = [];
-        
-        for($i = 0; $i < 10; $i++) {
-            $task = new \stdClass();
-            $task->name = 'task '.($i+1);
-            $tasks[] = $task;
-        }
+        $tasks = Task::orderBy('created_at', 'DESC')->get();
 
         $data = ['tasks' => $tasks];
+        
         return view('tasks.index', $data);
     }
 
