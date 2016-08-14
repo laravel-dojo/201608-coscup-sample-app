dec649cfd6328dd62808869620fe7b2f436f9dba
Shengyou Fan <shengyoufan@gmail.com>
2016-08-14 14:24:53 +0800

    安裝 Laravel Tracy Debugger

--- config/app.php
+++ config/app.php
@@ -148,6 +148,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Recca0120\LaravelTracy\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
